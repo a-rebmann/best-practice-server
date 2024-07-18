0c534214445f5d5d2d0e06710d1f045942539e31
Adrian Rebmann <rebmann@informatik.uni-mannheim.de>
2024-07-18 10:30:33 +0200

    fix sentence transformer setup

--- app/util/fileutils.py
+++ app/util/fileutils.py
@@ -20,26 +20,26 @@ def check_data_directories_on_start(conf: Config):
     if not os.path.exists(conf.DATA_DATASET):
         os.makedirs(conf.DATA_DATASET)
 
-    # check files in the logs directory and unzip if necessary
-    # for file in os.listdir(conf.DATA_LOGS):
-    #     if file.endswith(".zip"):
-    #         with ZipFile(conf.DATA_LOGS / file, 'r') as zip_ref:
-    #             zip_ref.extractall(conf.DATA_LOGS)
-    #         os.remove(conf.DATA_LOGS / file)
-    #
-    # # check the interim directory and unzip files in place if necessary
-    # for file in os.listdir(conf.DATA_INTERIM):
-    #     if file.endswith(".zip"):
-    #         with ZipFile(conf.DATA_INTERIM / file, 'r') as zip_ref:
-    #             zip_ref.extractall(conf.DATA_INTERIM)
-    #         os.remove(conf.DATA_INTERIM / file)
-    #
-    # # check the model directory and unzip files in place if necessary
-    # for file in os.listdir(conf.DATA_DATASET):
-    #     if file.endswith(".zip"):
-    #         with ZipFile(conf.DATA_DATASET / file, 'r') as zip_ref:
-    #             zip_ref.extractall(conf.DATA_DATASET)
-    #         os.remove(conf.DATA_DATASET / file)
+    check files in the logs directory and unzip if necessary
+    for file in os.listdir(conf.DATA_LOGS):
+        if file.endswith(".zip"):
+            with ZipFile(conf.DATA_LOGS / file, 'r') as zip_ref:
+                zip_ref.extractall(conf.DATA_LOGS)
+            os.remove(conf.DATA_LOGS / file)
+
+    # check the interim directory and unzip files in place if necessary
+    for file in os.listdir(conf.DATA_INTERIM):
+        if file.endswith(".zip"):
+            with ZipFile(conf.DATA_INTERIM / file, 'r') as zip_ref:
+                zip_ref.extractall(conf.DATA_INTERIM)
+            os.remove(conf.DATA_INTERIM / file)
+
+    # check the model directory and unzip files in place if necessary
+    for file in os.listdir(conf.DATA_DATASET):
+        if file.endswith(".zip"):
+            with ZipFile(conf.DATA_DATASET / file, 'r') as zip_ref:
+                zip_ref.extractall(conf.DATA_DATASET)
+            os.remove(conf.DATA_DATASET / file)
 
 
 
